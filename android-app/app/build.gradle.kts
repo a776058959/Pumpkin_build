4d24a8b410c1c6883830493dd98a403f84b289dd
a776058959 <776058959@qq.com>
2026-09-14 21:17:41 +0800

    build: migrate kotlinOptions to compilerOptions DSL; Gradle 8.14.4

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -56,12 +56,14 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     lint {
         abortOnError = false
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
     }
 }
 
